5d0b894bd21ee0557daa3c79483c34c2531533fa
Aki Unleash <aki.unleash@gmai.comm>
2021-08-04 22:19:01 +0900

    Implementaion of data registration process.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,70 @@
-import { Auth, Button, IconLogOut } from "@supabase/ui"
-import type { ReactNode } from "react"
-import React from "react";
+import { Auth, Button, IconLogOut } from "@supabase/ui";
+import React, { ReactNode, useCallback, useEffect, useState } from "react";
 import { LayoutWrapper } from "src/components/layoutWrapper";
+import { Title, TitleList } from "src/components/titleList";
 import { client } from "src/libs/supabase";
 
 type Props = {
   children: ReactNode;
-}
+};
+
+const getTitles = async () => {
+  const { data, error } = await client
+    .from("manage_title")
+    .select("*")
+    .order("title");
+  if (!error && data) {
+    return data;
+  }
+  return [];
+};
 
 const Container = (props: Props) => {
   const { user } = Auth.useUser();
 
+  const [text, setText] = useState<string>("");
+  const [titles, setTitles] = useState<Title[]>([]);
+
+  const getTitleList = useCallback(async () => {
+    const data = await getTitles();
+    setTitles(data);
+  }, []);
+
+  useEffect(() => {
+    getTitleList();
+  }, [user, getTitleList]);
+
   if (user) {
     return (
       <div>
+        <div className="flex justify-center gap-2 p-4">
+          <input
+            className="w-full h-12 px-4 bg-white border border-gray-300 rounded shadow appearance-none hover:border-gray-700"
+            placeholder="Filtering text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <TitleList
+          titles={titles}
+          uuid={user.id}
+          getTitleList={getTitleList}
+          filterText={text}
+        />
         <div className="flex justify-end mx-2 my-4">
           <Button
             size="medium"
             icon={<IconLogOut />}
-            onClick={() => client.auth.signOut()} >
+            onClick={() => client.auth.signOut()}
+          >
             Sign out
           </Button>
         </div>
       </div>
-    )
+    );
   }
-  return <>{props.children}</>
-}
+  return <>{props.children}</>;
+};
 
 const Home = () => {
   return (
@@ -45,7 +83,7 @@ const Home = () => {
         </Container>
       </Auth.UserContextProvider>
     </LayoutWrapper>
-  )
-}
+  );
+};
 
 export default Home;
